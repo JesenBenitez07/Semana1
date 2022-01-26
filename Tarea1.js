6c65314634730e2eddc19875077f7e2bc716427f
JesenBenitez07 <jarielescobar5s@gmail.com>
2022-01-25 22:18:03 -0600

    Version 2.0

--- Tarea1.js
+++ Tarea1.js
@@ -1,44 +1,17 @@
-let objetoEstudiante1 = {
-    id:"6172184",
-    nombre: 'Jesen',
-    ciudad: 'Choloma',
-    nota: 90,
-    observacion: "Aprobado"
-}
+const Estudiante = [
+    { id: 6172184, nombre: 'Jesen', ciudad: 'Choloma', nota: 90, observacion: 'Aprobado'},
+    { id: 61721485, nombre: 'Andrea', ciudad: 'SPS', nota: 94, observacion: 'Aprobado'},
+    { id: 61721486, nombre: 'Dania', ciudad: 'Choloma', nota: 95, observacion: 'Aprobado'},
+    { id: 61721487, nombre: 'Fernando', ciudad: 'SPS', nota: 70, observacion: 'Aprobado'},
+    { id: 61721488, nombre: 'Alejandro', ciudad: 'SPS', nota: 90, observacion: 'Aprobado'},
+    { id: 61721489, nombre: 'Fernando', ciudad: 'SPS', nota: 52, observacion: 'Reprobado'},
+    { id: 61721490, nombre: 'Glenda', ciudad: 'Choloma', nota: 87, observacion: 'Aprobado'},
+    { id: 61721491, nombre: 'Fabian', ciudad: 'Choloma', nota: 40, observacion: 'Reprobado'},
+    { id: 61721492, nombre: 'Fernanda', ciudad: 'SPS', nota: 54, observacion: 'Reprobado'},
+    { id: 61721493, nombre: 'Daniel', ciudad: 'SPS', nota: 89, observacion: 'Aprobado'}
+]
 
-let objetoEstudiante2 = {
-    id:"61721485",
-    nombre: 'Andrea',
-    ciudad: 'SPS',
-    nota: 94,
-    observacion: "Aprobado"
-}
-let objetoEstudiante3 = {
-    id:"61721486",
-    nombre: 'Dania',
-    ciudad: 'Choloma',
-    nota: 59,
-    observacion: "Reprobado"
-}
+const Aprobado = Estudiante.filter(item => {
+    return item.observacion === 'Aprobado'})
 
-let objetoEstudiante4 = {
-    id:"61721487",
-    nombre: 'Fernando',
-    ciudad: 'SPS',
-    nota: 70,
-    observacion: "Aprobado"
-}
-let arraynumeros = [1,25,34,56]
-let arrayEstudiante = [
-    {
-      id: '61721484', nombre: 'Jesen', ciudad: 'Choloma' , nota: 90, observacion: "Aprobado"
-    },
-    objetoEstudiante1
- ]
-
-arrayEstudiante.push(objetoEstudiante3)
-arrayEstudiante.push(50)
-
-let resultadoFiltro = arrayEstudiante.find(estudiante => estudiante.observacion === 'Reprobado')
-
-console.log(resultadoFiltro)
+console.log(Aprobado)
